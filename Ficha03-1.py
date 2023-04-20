b1b6d288f60886153d84bc85c52efde6c4f69411
JuansoidePP <juanpereiraignacioprado42@gmail.com>
2023-04-20 18:34:58 -0300

    Esto es un commit

--- Ficha03-1.py
+++ Ficha03-1.py
@@ -1,5 +1,10 @@
+# Plazo Fijo
+
+#Ingresar dinero por teclado
 din = float(input("Deme su dinero: "))
 
+#Interes del banco menos el dinero por servicios financieros
 interes = din * 0.023 + din - 20
 
-print("Aqui esta ru dineros:", interes)
+print("Aqui esta tu dinero:", interes)
+
