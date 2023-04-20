859c234c2a0c0d7ebe1092b688450a08997d6be5
JuansoidePP <juanpereiraignacioprado42@gmail.com>
2023-04-20 20:08:14 -0300

    Ejercicio 4 - Dificil de hacer sin condicionales

---
+++ Ficha03-4.py
@@ -0,0 +1,29 @@
+#Definir duracion de viaje sabiendo el horario de salida y de llegada
+
+salida = input("Ingrese el horario de salida de forma hh:mm: ")
+
+llegada = input("Ingrese el horario de llegada de forma hh:mm: ")
+
+#Separar y convertir horas y minutos de salida
+sepHoraSal = int(salida[0]+salida[1])
+convertirHSalida = sepHoraSal * 60
+
+minutosSalida = int(salida[3]+salida[4]) + convertirHSalida
+
+
+#Separar y convertir horas y minutos de llegada
+sepHoraLleg = int(llegada[0]+llegada[1])
+
+convertirHLleg = sepHoraLleg * 60
+minutosLlegada = int(llegada[3]+llegada[4]) + convertirHLleg
+
+
+#Duracion en horas
+horas = int((convertirHLleg - convertirHSalida)/60)
+
+#Duracion en minutos
+minutos = int(minutosLlegada - minutosSalida)
+
+print("duracion de vuelo", minutos, "minutos")
+
+#Al no usar condicionales, este algoritmo esta mal
