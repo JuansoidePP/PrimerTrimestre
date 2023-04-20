70760a6bc96664eb2f63e80e42e6e409e31a405c
JuansoidePP <juanpereiraignacioprado42@gmail.com>
2023-04-20 18:53:21 -0300

    Ejercicio 2

---
+++ Ficha03-2.py
@@ -0,0 +1,5 @@
+#Ingresar fecha de nacimiento
+cad = input("Ingrese su fecha de nacimiento de la forma dd/mm/aaaa: ")
+
+#Separar la cadena
+print("Dia: ", cad[0] + cad[1], "- Mes: ", cad[3] + cad[4], "- Año:",cad[6]+cad[7]+cad[8]+cad[9])
